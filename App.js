7fd6455eb846baf8bdae4005a3e238c5c3f70eb0
Erotokritos Dampasis <kritosdab@gmail.com>
2024-05-13 18:58:38 +0300

    create app ogdoo

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import WebView from 'react-native-webview';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const SiteUrl = 'http://www.ogdoo.gr';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{padding: 30 }}/>
       <StatusBar style="auto" />
+      <View style={{ width: '100%', height: '100%' }}>
+        <WebView
+          ref={(ref) => { this.webviewRef = ref; }}
+          source={{ uri: SiteUrl }}
+        />
+      </View>
     </View>
   );
 }
